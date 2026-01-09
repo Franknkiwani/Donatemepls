3643c63d22cbc3237162321dc8778a10d58a5a0e
Franknkiwani <132098616+Franknkiwani@users.noreply.github.com>
2026-01-10 00:29:36 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,4 @@
+// --- FIREBASE CORE CONFIGURATION ---
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-database.js";
@@ -10,6 +11,19 @@ const firebaseConfig = {
     appId: "1:1094792075584:web:d49e9c3f899d3cd31082a5"
 };
 
+// Initialize Firebase Services
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getDatabase(app);
+
+// --- GLOBAL SYSTEM CONSTANTS ---
+// Centralizing these here makes your other scripts much cleaner!
+export const ADMIN_UID = "4xEDAzSt5javvSnW5mws2Ma8i8n1";
+export const PAYPAL_PLAN_ID = 'P-47S21200XM2944742NFPLPEA';
+
+// Preset Avatars used across the app
+export const AVATAR_PRESETS = [
+    "https://img.pikbest.com/origin/10/25/30/74apIkbEsT5qB.jpg!w700wp",
+    "https://thumbs.dreamstime.com/b/cool-neon-party-wolf-headphones-black-background-colorful-illustration-music-theme-modern-portrait-bright-vibrant-385601082.jpg",
+    "https://wallpapers.com/images/hd/gaming-profile-pictures-tt8bbzdcf6zibhoi.jpg"
+];
